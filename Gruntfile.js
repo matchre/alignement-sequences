49db548456f4d3087f68e37569c1c75caf7f92a1
Neurolit <neurolit@gmail.com>
2014-11-06 20:15:30 +0100

    Add alignement.js in Grunt JSlint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function (grunt) {
 
         jslint: {
             files: [
-                'meta.json'
+                'meta.json',
+                'alignement.js'
             ],
             options: {
                 failOnError: true
@@ -25,9 +26,9 @@ module.exports = function (grunt) {
     // Travis CI task.
     grunt.registerTask('test', 'jslint');
 
-    grunt.registerTask('meta_json_pretty_print', function() {
-        var metadata = grunt.file.readJSON('meta.json') ;
-        grunt.file.write('meta.json',JSON.stringify(metadata, null, 4));
-    })
+    grunt.registerTask('meta_json_pretty_print', function () {
+        var metadata = grunt.file.readJSON('meta.json');
+        grunt.file.write('meta.json', JSON.stringify(metadata, null, 4));
+    });
 
 };
